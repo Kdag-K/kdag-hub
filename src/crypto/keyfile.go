d1d0d5863ac3e5d414c632ac9570fb10f7e48132
bwhour <juvemeen.xio@gmail.com>
2021-09-02 10:49:25 +0800

    add api EncryptKey

--- src/crypto/keyfile.go
+++ src/crypto/keyfile.go
@@ -1,6 +1,13 @@
 package crypto
 
-import "github.com/ethereum/go-ethereum/accounts/keystore"
+import (
+	"encoding/hex"
+	"encoding/json"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/ethereum/go-ethereum/crypto"
+)
 
 const ethereumVersion = 3
 
@@ -21,4 +28,22 @@ type EncryptedKeyJSONKnode struct {
 	Crypto    keystore.CryptoJSON `json:"crypto"`
 	ID        string              `json:"id"`
 	Version   int                 `json:"version"`
+}
+
+// EncryptKey encrypts a key using the specified scrypt parameters into a json
+// blob that can be decrypted later on.
+func EncryptKey(key *keystore.Key, auth string, scryptN, scryptP int) ([]byte, error) {
+	keyBytes := math.PaddedBigBytes(key.PrivateKey.D, 32)
+	cryptoStruct, err := keystore.EncryptDataV3(keyBytes, []byte(auth), scryptN, scryptP)
+	if err != nil {
+		return nil, err
+	}
+	encryptedKeyJSONKnode := EncryptedKeyJSONKnode{
+		hex.EncodeToString(key.Address[:]),
+		hex.EncodeToString(crypto.FromECDSAPub(&key.PrivateKey.PublicKey)),
+		cryptoStruct,
+		key.Id.String(),
+		ethereumVersion,
+	}
+	return json.Marshal(encryptedKeyJSONKnode)
 }
