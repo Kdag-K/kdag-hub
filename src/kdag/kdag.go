e2b33e8cf55a3e5a0760a72051507d8543f77559
bwhour <juvemeen.xio@gmail.com>
2021-09-17 14:53:14 +0800

    add Inmem kdag node

--- src/kdag/kdag.go
+++ src/kdag/kdag.go
@@ -1,1 +1,27 @@
 package kdag
+
+import (
+	"github.com/Kdag-K/kdag/src/kdag"
+	kdagconf "github.com/Kdag-K/kdag/src/config"
+	"github.com/Kdag-K/evm/src/service"
+	"github.com/Kdag-K/evm/src/state"
+	"github.com/sirupsen/logrus"
+)
+
+// InmemBabble implementes EVM's Consensus interface.
+// It uses an inmemory Kdag node.
+type InmemKdag struct {
+	config      *kdagconf.Config
+	kdag       *kdag.Kdag
+	ethService *service.Service
+	ethState   *state.State
+	logger     *logrus.Entry
+}
+
+// NewInmemKdag instantiates a new InmemKdag consensus system
+func NewInmemKdag(config *kdagconf.Config, logger *logrus.Entry) *InmemKdag {
+	return &InmemKdag{
+		config: config,
+		logger: logger,
+	}
+}
