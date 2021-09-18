8ece9fea8c5baa634bdd186c99364641f836ff1c
bwhour <juvemeen.xio@gmail.com>
2021-09-18 10:51:57 +0800

    add ShowIPWarnings into utils package

--- cmd/knode/commands/config/utils.go
+++ cmd/knode/commands/config/utils.go
@@ -18,3 +18,24 @@ func CreateMonetConfigFolders(configDir string) error {
 		filepath.Join(configDir, configuration.EthDir, configuration.POADir),
 	})
 }
+
+// ShowIPWarnings outputs warnings if IP addresses are local and propably not
+// reachable from the outside.
+func ShowIPWarnings() {
+	api := configuration.Global.APIAddr
+	listen := configuration.Global.Kdag.BindAddr
+	advertise := configuration.Global.Kdag.AdvertiseAddr
+	
+	if common.CheckIP(api, true) {
+		common.MessageWithType(common.MsgWarning, fmt.Sprintf("Knode service API address in knode.toml may be internal: %s", api))
+	}
+	
+	if advertise != "" && common.CheckIP(advertise, false) {
+		common.MessageWithType(common.MsgWarning, fmt.Sprintf("Kdag.advertise address in knode.toml may be internal: %s \n", listen))
+	} else if common.CheckIP(listen, false) {
+		common.MessageWithType(
+			common.MsgWarning,
+			fmt.Sprintf("kdag.listen address in knode.toml may be internal: %s. Consider setting an advertise address.", listen),
+		)
+	}
+}
