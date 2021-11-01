2eaadfaa222873c70022874f8538de06fb035ae9
hi-bwhour <juvemeen.xio@gmail.com>
2021-11-01 15:52:39 +0800

    add dogye cmd option

--- cmd/dogye/commands/keys/keys.go
+++ cmd/dogye/commands/keys/keys.go
@@ -1,1 +1,30 @@
 package keys
+
+import (
+	"github.com/Kdag-K/kdag-hub/src/configuration"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	_keystore = configuration.DefaultKeystoreDir()
+)
+
+//KeysCmd is an Ethereum key manager
+var KeysCmd = &cobra.Command{
+	Use:              "keys",
+	Short:            "Knode key manager",
+	TraverseChildren: true,
+}
+
+func init() {
+	//Subcommands
+	KeysCmd.AddCommand(
+		newImportCmd(),
+		newGenerateCmd(),
+	)
+	
+	//Commonly used command line flags
+	KeysCmd.PersistentFlags().StringVarP(&_keystore, "keystore", "k", _keystore, "keystore directory")
+	viper.BindPFlags(KeysCmd.Flags())
+}
