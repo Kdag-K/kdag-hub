102758a910a7a0e22f17bb209a5f13da921aebcf
hi-bwhour <juvemeen.xio@gmail.com>
2021-11-17 14:24:23 +0800

    add network type definiton

---
+++ cmd/dogye/network/types.go
@@ -0,0 +1,37 @@
+package network
+
+// node is a struct for defining individual nodes in a network.
+type node struct {
+	Moniker   string `toml:"moniker"`
+	NetAddr   string `toml:"netaddr"`
+	Validator bool   `toml:"validator"`
+	Tokens    string `toml:"tokens"`
+	PubKeyHex string `toml:"pubkey"`
+	Address   string `toml:"address"`
+	NonNode   bool   `toml:"nonnode"`
+}
+
+//Config defines the nodes.toml file structure
+type Config struct {
+	Network networkConfig `toml:"network"`
+	Docker  dockerConfig  `toml:"docker"`
+	Nodes   []node        `toml:"nodes,squash"`
+}
+
+type networkConfig struct {
+	Name string `toml:"name"`
+}
+
+type dockerConfig struct {
+	Name    string `toml:"name"`
+	BaseIP  string `toml:"baseip"`
+	Subnet  string `toml:"subnet"`
+	IPRange string `toml:"iprange"`
+	Gateway string `toml:"gateway"`
+	LastIP  string `toml:"lastip"`
+}
+
+type dockerNodeConfig struct {
+	Moniker string `toml:"moniker"`
+	NetAddr string `toml:"netaddr"`
+}
