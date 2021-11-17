178ca81d2690579edc74ef5f9f8bbb6f3d616d7e
hi-bwhour <juvemeen.xio@gmail.com>
2021-11-17 14:41:23 +0800

    update generate transaction command

--- cmd/dogye/transactions/generate.go
+++ cmd/dogye/transactions/generate.go
@@ -3,12 +3,16 @@ package transactions
 import (
 	"bufio"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"math/big"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
-	
+	"github.com/BurntSushi/toml"
+    config "github.com/Kdag-K/kdag-hub/src/configuration"
+	"github.com/Kdag-K/kdag-hub/cmd/dogye/network"
 	"github.com/Kdag-K/kdag-hub/src/common"
 	"github.com/Kdag-K/kdag-hub/src/files"
 	"github.com/spf13/cobra"
@@ -96,6 +100,105 @@ func generateTransactions(cmd *cobra.Command, args []string) error {
 	if err := files.CreateDirsIfNotExists([]string{transDir}); err != nil {
 		return err
 	}
+	faucetTransFile := filepath.Join(transDir, "faucet.json")
+	transFile := filepath.Join(transDir, "trans.json")
+	deltaFile := filepath.Join(transDir, "delta.json")
+	
+	var conf = network.Config{}
+	
+	tomlbytes, err := ioutil.ReadFile(networkTomlFile)
+	if err != nil {
+		return fmt.Errorf("Failed to read the toml file at '%s': %v", networkTomlFile, err)
+	}
+	
+	err = toml.Unmarshal(tomlbytes, &conf)
+	if err != nil {
+		return nil
+	}
+	
+	if ips != "" {
+		ipmap, err = loadIPS()
+		if err != nil {
+			return err
+		}
+	}
+	
+	var nodes []node
+	var accounts []account
+	var debits []*big.Int
+	var credits []*big.Int
+	var faucetAccount *account
+	var trans []transaction
+	var deltas []delta
+	var nodeTrans []nodeTransactions
+	common.DebugMessage("Parsing network.toml for node and accounts")
+	for _, n := range conf.Nodes {
+		
+		netaddr := n.NetAddr
+		moniker := n.Moniker
+		balance := n.Tokens
+		
+		if netaddr == "" {
+			if moniker == faucet {
+				faucetAccount = &account{
+					Address:   n.Address,
+					Moniker:   n.Moniker,
+					PubKeyHex: n.PubKeyHex,
+					Tokens:    balance,
+				}
+				
+				common.DebugMessage("faucet ", faucetAccount.Moniker, balance)
+			} else {
+				accounts = append(accounts, account{
+					Address:   n.Address,
+					Moniker:   n.Moniker,
+					PubKeyHex: n.PubKeyHex,
+					Tokens:    balance,
+				})
+				
+				credits = append(credits, new(big.Int))
+				debits = append(debits, new(big.Int))
+				
+				common.DebugMessage("account ", moniker, balance)
+			}
+		} else {
+			if ipmap != nil {
+				netaddr = ipmap[netaddr]
+			}
+			if !strings.Contains(netaddr, ":") && len(netaddr) > 0 {
+				netaddr += config.DefaultAPIAddr
+			}
+			nodes = append(nodes, node{
+				NetAddr: netaddr,
+				Moniker: moniker})
+			common.DebugMessage("node ", moniker)
+		}
+		
+	}
+	
+	nodecnt := len(nodes)
+	accountcnt := len(accounts)
+	
+	common.InfoMessage("Read " + strconv.Itoa(nodecnt) + " nodes.")
+	common.InfoMessage("Read " + strconv.Itoa(accountcnt) + " accounts.")
+	
+	if faucetAccount == nil {
+		return errors.New("faucet account not found: " + faucet)
+	}
+	
+	if accountcnt < 2 {
+		return errors.New("you must have at least 2 accounts")
+	}
+	
+	common.InfoMessage("Faucet account, " + faucet + ", found.")
+	
+	for i := 0; i < accountcnt; i++ {
+		nodeTrans = append(nodeTrans, nodeTransactions{
+			Address: accounts[i].Address,
+			Moniker: accounts[i].Moniker,
+		})
+	}
+	
 	
 	return nil
 }
