ce170743d7f9032dcffd8efeedeb2e510cb585e9
hi-bwhour <juvemeen.xio@gmail.com>
2021-11-18 16:11:28 +0800

    add network config options

--- cmd/dogye/network/network.go
+++ cmd/dogye/network/network.go
@@ -1,1 +1,32 @@
 package network
+
+import (
+	"path/filepath"
+	
+	"github.com/Kdag-K/kdag-hub/cmd/dogye/configuration"
+	"github.com/Kdag-K/kdag-hub/src/files"
+)
+
+const (
+	dogyeNetworksDir  = "networks"
+	dogyeKeystoreDir  = "keystore"
+	dogyeDockerDir    = "docker"
+	dogyeTmpDir       = ".tmp"
+	defaultTokens       = "1234567890000000000000"
+	networkTomlFileName = "network.toml"
+)
+
+var (
+	networkName = "network0"
+)
+
+
+func createdogyeRootNetworkFolders() error {
+	
+	files.CreateDirsIfNotExists([]string{
+		configuration.DogyeConfigDir,
+		filepath.Join(configuration.DogyeConfigDir, dogyeNetworksDir),
+	})
+	
+	return nil
+}
