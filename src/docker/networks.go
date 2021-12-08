109a59b1c325118d68a50f0eb00947f7c874d42f
hi-bwhour <juvemeen.xio@gmail.com>
2021-12-08 15:11:01 +0800

    add remove action

--- src/docker/networks.go
+++ src/docker/networks.go
@@ -3,28 +3,34 @@ package docker
 import (
 	"context"
 	"fmt"
-
+	
 	"github.com/moby/moby/api/types"
 	"github.com/moby/moby/client"
 )
 
 // GetNetworks lists networks.
 func GetNetworks(cli *client.Client, output bool) (map[string]string, error) {
-
+	
 	rtn := make(map[string]string)
-
+	
 	ctx := context.Background()
 	arrRes, err := cli.NetworkList(ctx, types.NetworkListOptions{})
 	if err != nil {
 		return rtn, err
 	}
-
+	
 	for _, net := range arrRes {
 		if output {
 			fmt.Printf("%s   %s  %s\n", net.Name, net.ID, net.Driver)
 		}
 		rtn[net.Name] = net.ID
 	}
-
+	
 	return rtn, nil
+}
+
+// RemoveNetwork removes a network
+func RemoveNetwork(cli *client.Client, networkID string) error {
+	ctx := context.Background()
+	return cli.NetworkRemove(ctx, networkID)
 }
