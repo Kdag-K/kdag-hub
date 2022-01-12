2632f108674a264b4b7fbeac5cf4659604634003
hi-bwhour <juvemeen.xio@gmail.com>
2022-01-12 10:48:19 +0800

    update stop command lie

--- cmd/dogye/commands/network/stop.go
+++ cmd/dogye/commands/network/stop.go
@@ -8,17 +8,38 @@ import (
 
 var stopAndDelete = false
 
+// stop command line.
+func newStopCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "stop [network] [node] ",
+		Short: "stop a network",
+		Long: `
+dogye network stop
+
+If the <node> value is provided, this command stops just that node. Otherwise it
+stops all the nodes. Additionaly, if the --remove flag is set, the nodes are 
+also deleted.
+		`,
+		Args: cobra.MinimumNArgs(1),
+		RunE: networkStop,
+	}
+	
+	addStopFlags(cmd)
+	
+	return cmd
+}
+
 func addStopFlags(cmd *cobra.Command) {
 	cmd.Flags().BoolVar(&stopAndDelete, "remove", stopAndDelete, "stop and remove node")
 	viper.BindPFlags(cmd.Flags())
 }
 
 func networkStop(cmd *cobra.Command, args []string) error {
-
+	
 	if len(args) == 1 { // Network
 		return docker.StopNetwork(args[0], stopAndDelete)
 	}
-
+	
 	return docker.StopNode(args[0], args[1], stopAndDelete)
-
+	
 }
