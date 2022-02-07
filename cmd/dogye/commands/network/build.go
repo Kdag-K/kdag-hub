98fe7bf822cc1cb96b736506ae5cc1991c99ba1e
xiaoming <xiaoming@new4g.cn>
2022-02-07 18:33:42 +0800

    add build option functions

--- cmd/dogye/commands/network/build.go
+++ cmd/dogye/commands/network/build.go
@@ -1,1 +1,84 @@
 package network
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+
+	"github.com/Kdag-K/kdag-hub/src/genesis"
+	bpeers "github.com/mosaicnetworks/babble/src/peers"
+
+	"github.com/Kdag-K/kdag-hub/cmd/giverny/configuration"
+	"github.com/Kdag-K/kdag-hub/src/common"
+	monetconfig "github.com/Kdag-K/kdag-hub/src/configuration"
+	"github.com/Kdag-K/kdag-hub/src/files"
+	"github.com/pelletier/go-toml"
+	"github.com/spf13/cobra"
+)
+
+func newBuildCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "build [network_name]",
+		Short: "build knode configuration files based on a dogye network file",
+		Args:  cobra.ExactArgs(1),
+		RunE:  networkBuild,
+	}
+	return cmd
+}
+
+func networkBuild(cmd *cobra.Command, args []string) error {
+	return buildNetwork(strings.TrimSpace(args[0]))
+}
+
+// buildNetwork builds the network. It is called directly from the "new" command
+// as well.
+func buildNetwork(networkName string) error {
+	if !common.CheckMoniker(networkName) {
+		return errors.New("network name, " + networkName + ", is invalid")
+	}
+
+	// Check all the files and directories we expect actually exist
+	thisNetworkDir := filepath.Join(configuration.GivernyConfigDir, givernyNetworksDir, networkName)
+	if !files.CheckIfExists(thisNetworkDir) {
+		return errors.New("cannot find the configuration folder, " + thisNetworkDir + " for " + networkName)
+	}
+
+	networkTomlFile := filepath.Join(thisNetworkDir, networkTomlFileName)
+	if !files.CheckIfExists(networkTomlFile) {
+		return errors.New("cannot find the configuration file: " + networkTomlFile)
+	}
+
+	var conf = Config{}
+
+	tomlbytes, err := ioutil.ReadFile(networkTomlFile)
+	if err != nil {
+		return fmt.Errorf("Failed to read the toml file at '%s': %v", networkTomlFile, err)
+	}
+
+	err = toml.Unmarshal(tomlbytes, &conf)
+	if err != nil {
+		return fmt.Errorf("Failed to parse the toml file at '%s': %v", networkTomlFile, err)
+	}
+
+	common.DebugMessage("Building network " + networkName)
+
+	err = generateKnodeConfig(&conf, thisNetworkDir)
+	if err != nil {
+		common.ErrorMessage("Error writing peers json file")
+		return err
+	}
+
+	return nil
+}
+
+func generateKnodeConfig(conf *Config, thisNetworkDir string) error {
+
+	return err
+}
+
+func generateKdagFiles(configDir string, peers []*bpeers.Peer) error {
+	return nil
+}
