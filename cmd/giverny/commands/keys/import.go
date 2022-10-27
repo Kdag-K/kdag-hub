52b1b80cbd6b84815ab5b42d25b509a0406d9781
K-dag <github@kdag.io>
2022-10-27 16:40:58 +0800

    newImportCmd command creates an Ethereum keyfile

---
+++ cmd/giverny/commands/keys/import.go
@@ -0,0 +1,34 @@
+package keys
+
+import (
+	monetcrypto "github.com/Kdag-K/kdag-hub/src/crypto"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var _passwordFile string
+
+//newImportCmd returns the command that creates a Ethereum keyfile
+func newImportCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "import [moniker] [keyfile]",
+		Short: "create an encrypted keyfile from an existing private key",
+		Args:  cobra.ExactArgs(2),
+		RunE:  importKey,
+	}
+
+	cmd.Flags().StringVar(&_passwordFile, "passfile", "", "the file that contains the passphrase for the keyfile")
+
+	viper.BindPFlags(cmd.Flags())
+
+	return cmd
+}
+
+func importKey(cmd *cobra.Command, args []string) error {
+	moniker := args[0]
+	privateKeyfile := args[1]
+
+	_, err := monetcrypto.NewKeyfileFull(_keystore, moniker, _passwordFile, privateKeyfile, false)
+
+	return err
+}
